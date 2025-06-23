60a1ecab8cecf77fd32d743877a67f2bf6786909
Russian-mafia1 <ianokipchirchir@gmail.com>
2025-06-24 01:15:12 +0300

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363401087525436@newsletter',
+                    newsletterName: 'HACKLINK',
                     serverMessageId: -1
                 }
             }
