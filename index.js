cdcccb44051af0938d7dd3734ccb20f7ab878861
Russian-mafia1 <ianokipchirchir@gmail.com>
2025-06-24 00:34:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /**
- * Knight Bot - A WhatsApp Bot
- * Copyright (c) 2024 Professor
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the MIT License.
- * 
- * Credits:
- * - Baileys Library by @adiwajshing
- * - Pair Code implementation inspired by TechGod143 & DGXEON
+ * y
+ * 5n 6
  */
 require('./settings')
 const { Boom } = require('@hapi/boom')
